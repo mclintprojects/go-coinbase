6a72193dc49920ae810e2199650ebc4b4ac26f94
Clinton Mbah <clintonmbah44@gmail.com>
2020-07-31 05:58:23 +0000

    minor fixes

--- transactions.go
+++ transactions.go
@@ -58,114 +58,115 @@ Example Response:
 
 */
 type APITransactionsDataDetails struct {
-  Title string
-  Subtitle string
+	Title    string
+	Subtitle string
 }
 type APITransactionsDataNetwork struct {
-  Status string
-  Name string
+	Status string
+	Name   string
 }
-type APITransactionsData  struct {
-  Id string
-  Type string
-  Status string
-  Amount APIBalance
-  Native_amount APIBalance
-  Description string
-  Created_at string
-  Updated_at string
-  Resource string
-  Resource_path string
-  Network APITransactionsDataNetwork
-  To APIResource
-  Buy *APIResource `json:"buy,omitempty"`
-  Sell *APIResource `json:"sell,omitempty"`
-  Send *APIResource `json:"send,omitempty"`
-  Request *APIResource `json:"request,omitempty"`
-  Transfer *APIResource `json:"transfer,omitempty"`
-  Fiat_deposit *APIResource `json:"fiat_deposit,omitempty"`
-  Fiat_withdrawal *APIResource `json:"fiat_withdrawal,omitempty"`
-  Exchange_deposit *APIResource `json:"exchange_deposit,omitempty"`
-  Exchange_withdrawal *APIResource `json:"exchange_withdrawal,omitempty"`
-  Vault_withdrawal *APIResource `json:"vault_withdrawal,omitempty"`
-  Instant_exchange bool
-  Details APITransactionsDataDetails
+type APITransactionsData struct {
+	Id                  string
+	Type                string
+	Status              string
+	Amount              APIBalance
+	Native_amount       APIBalance
+	Description         string
+	Created_at          string
+	Updated_at          string
+	Resource            string
+	Resource_path       string
+	Network             APITransactionsDataNetwork
+	To                  APIResource
+	Buy                 *APIResource `json:"buy,omitempty"`
+	Sell                *APIResource `json:"sell,omitempty"`
+	Send                *APIResource `json:"send,omitempty"`
+	Request             *APIResource `json:"request,omitempty"`
+	Transfer            *APIResource `json:"transfer,omitempty"`
+	Fiat_deposit        *APIResource `json:"fiat_deposit,omitempty"`
+	Fiat_withdrawal     *APIResource `json:"fiat_withdrawal,omitempty"`
+	Exchange_deposit    *APIResource `json:"exchange_deposit,omitempty"`
+	Exchange_withdrawal *APIResource `json:"exchange_withdrawal,omitempty"`
+	Vault_withdrawal    *APIResource `json:"vault_withdrawal,omitempty"`
+	Instant_exchange    bool
+	Details             APITransactionsDataDetails
 }
 type APITransactions struct {
-  Pagination APIPagination
-  Data []APITransactionsData
-  Errors []Error
+	Pagination APIPagination
+	Data       []APITransactionsData
+	Errors     []Error
 }
 type APITransaction struct {
-  Data APITransactionsData
-  Errors []Error
+	Data   APITransactionsData
+	Errors []Error
 }
+
 // GetTransactions requires an account ID and returns an APITransactions struct
 func (a *APIClient) GetTransactions(id string) (trans APITransactions, err error) {
-  err = a.Fetch("GET", "/v2/accounts/" + id + "/transactions", nil, &trans)
-  if err != nil {
-    return
-  }
-  return
+	err = a.Fetch("GET", "/v2/accounts/"+id+"/transactions", nil, &trans)
+	if err != nil {
+		return
+	}
+	return
 }
 
 // GetTransaction requires an account ID, transaction ID and returns an APITransaction struct
 func (a *APIClient) GetTransaction(id, transId string) (trans APITransaction, err error) {
-  path := "/v2/accounts/" + id + "/transactions/" + transId
-  err = a.Fetch("GET", path, nil, &trans)
-  if err != nil {
-    return
-  }
-  return
+	path := "/v2/accounts/" + id + "/transactions/" + transId
+	err = a.Fetch("GET", path, nil, &trans)
+	if err != nil {
+		return
+	}
+	return
 }
 
 type APITransactionsSend struct {
-  Type string `json:"type"`
-  To string `json:"to"`
-  Amount float64 `json:"amount"`
-  Currency string `json:"currency"`
-  Fee string `json:"fee"`
+	Type     string  `json:"type"`
+	To       string  `json:"to"`
+	Amount   float64 `json:"amount"`
+	Currency string  `json:"currency"`
+	Fee      string  `json:"fee,omitempty"`
+	Idem     string  `json:"idem,omitempty"`
 }
-
 
 // SendTransferRequestMoney requires an account ID, APITransactionsSend struct
 // and returns am APITransaction struct
 // TODO move account ID into the APITransactionsSend struct
 func (a *APIClient) SendTransferRequestMoney(id string, send APITransactionsSend) (trans APITransaction, err error) {
-  // TODO implement idem
-  err = a.Fetch("POST", "/v2/accounts/" + id + "/transactions", send, &trans)
-  if err != nil {
-    return
-  }
-  return
+	// TODO implement idem
+	err = a.Fetch("POST", "/v2/accounts/"+id+"/transactions", send, &trans)
+	if err != nil {
+		return
+	}
+	return
 }
 
 // CompleteRequestMoney requires an account ID, transaction ID
 func (a *APIClient) CompleteRequestMoney(id, transId string) (err error) {
-  path := "/v2/accounts/" + id + "/transactions/" + transId + "/complete"
-  err = a.Fetch("GET", path, nil, nil)
-  if err != nil {
-    return
-  }
-  return
+	path := "/v2/accounts/" + id + "/transactions/" + transId + "/complete"
+	err = a.Fetch("GET", path, nil, nil)
+	if err != nil {
+		return
+	}
+	return
 }
 
 // ResendRequestMoney requires an account ID, transaction ID
 func (a *APIClient) ResendRequestMoney(id, transId string) (err error) {
-  path := "/v2/accounts/" + id + "/transactions/" + transId + "/resend"
-  err = a.Fetch("GET", path, nil, nil)
-  if err != nil {
-    return
-  }
-  return
+	path := "/v2/accounts/" + id + "/transactions/" + transId + "/resend"
+	err = a.Fetch("GET", path, nil, nil)
+	if err != nil {
+		return
+	}
+	return
 }
 
 // CancelRequestMoney requires an account ID, transaction ID
 func (a *APIClient) CancelRequestMoney(id, transId string) (err error) {
-  path := "/v2/accounts/" + id + "/transactions/" + transId
-  err = a.Fetch("DELETE", path, nil, nil)
-  if err != nil {
-    return
-  }
-  return
+	path := "/v2/accounts/" + id + "/transactions/" + transId
+	err = a.Fetch("DELETE", path, nil, nil)
+	if err != nil {
+		return
+	}
+	return
 }
